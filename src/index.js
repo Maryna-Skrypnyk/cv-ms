b01317edce184b8c967e956093b33562b3306344
Maryna Skrypnyk <74303774+Maryna-Skrypnyk@users.noreply.github.com>
2023-04-20 13:02:03 +0300

    updates lang-switcher

--- src/index.js
+++ src/index.js
@@ -2,3 +2,12 @@
 import './js/theme-switcher.js';
 import './js/lang-switcher.js';
 // import './js/lang-data.js';
+
+// if (typeof require !== 'undefined') {
+//   var customSelect = require('custom-select').default;
+//   require('../node_modules/custom-select/build/custom-select.css');
+// }
+
+// const mySelects = customSelect('select');
+
+// console.log(mySelects);
