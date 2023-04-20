ecd08e761a29b5c95bb2a9e3499bb32a94cb5594
Maryna Skrypnyk <74303774+Maryna-Skrypnyk@users.noreply.github.com>
2023-04-20 15:25:52 +0300

    back-to-top

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-// import './sass/main.scss';
+import './sass/main.scss';
 import './js/theme-switcher.js';
 import './js/lang-switcher.js';
+import './js/back-to-top.js';
 // import './js/lang-data.js';
