272a44a1d6972a55931fa0a8cb61c6a3438d8d7b
Maryna-Skrypnyk <marina.skrypnyk@gmail.com>
2023-04-24 19:50:25 +0300

    change select language custom

--- src/js/lang-switcher.js
+++ src/js/lang-switcher.js
@@ -1,44 +1,45 @@
 import { langArr } from './lang-data.js';
-const selectMob = document.getElementById('select-mob');
-const select = document.getElementById('standard-select');
-
 const allLang = ['en', 'ua'];
 
-selectMob.addEventListener('change', changeURLLanguageMob);
-select.addEventListener('change', changeURLLanguage);
+const selectHeader = document.querySelectorAll('.select__header');
+const selectItem = document.querySelectorAll('.select__item');
 
-// перенаправлення на url з індикатором мови
+selectHeader.forEach(item => {
+  item.addEventListener('click', selectToggle);
+});
 
-function changeURLLanguageMob() {
-  let lang = selectMob.value;
+selectItem.forEach(item => {
+  item.addEventListener('click', selectChoose);
+});
+
+function selectToggle() {
+  this.parentElement.classList.toggle('is-active');
+}
+
+function selectChoose() {
+  const text = this.innerText,
+    select = this.closest('.select'),
+    currentText = select.querySelector('.select__current');
+  currentText.innerText = text;
+
+  select.classList.remove('is-active');
+
+  changeURLLanguage(text);
+}
+
+//change LanguageURL
+function changeURLLanguage(option) {
+  const lang = option.toLowerCase();
   location.href = window.location.pathname + '#' + lang;
   location.reload();
 }
 
-function changeURLLanguage() {
-  let lang = select.value;
-  location.href = window.location.pathname + '#' + lang;
-  location.reload();
-}
-
-function changeLanguage() {
-  let hash = window.location.hash;
-  hash = hash.substring(1);
-  if (!allLang.includes(hash)) {
-    location.href = window.location.pathname + '#en';
-    location.reload();
-  }
-  select.value = hash;
-  selectMob.value = hash;
-
+//translate content
+function translateContent(hash) {
   document.querySelector('title').innerHTML = langArr['title'][hash];
   for (let key in langArr) {
-    // let elem = document.querySelector('.lng-' + key);
     let elemAll = document.querySelectorAll('.lng-' + key);
 
-    // if (elem) {
-    //   elem.innerHTML = langArr[key][hash];
-    // }
     if (elemAll.length > 0) {
       elemAll.forEach(el => {
         if (langArr[key][hash]) {
@@ -48,6 +49,23 @@ function changeLanguage() {
       });
     }
   }
+}
+
+//change language current
+function changeLanguage() {
+  let hash = window.location.hash;
+  hash = hash.substring(1);
+  if (!allLang.includes(hash)) {
+    location.href = window.location.pathname + '#en';
+    location.reload();
+  }
+
+  const currentText = document.querySelectorAll('.select__current');
+  currentText.forEach(item => {
+    item.innerText = hash.toUpperCase();
+  });
+
+  translateContent(hash);
 }
 
 changeLanguage();
